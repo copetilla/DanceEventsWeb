534ddeeb925c99de123f7e55c568ae0bb2c05768
copetilla <gabrielgz2001@gmail.com>
2024-05-21 17:01:53 -0400

    ready for deploy

--- my-app/app/(root)/page.tsx
+++ my-app/app/(root)/page.tsx
@@ -1,5 +1,6 @@
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -9,7 +10,27 @@ export default function Home() {
           <div className="flex flex-col justify-center gap-8">
             <h1 className='h1-bold'>Host, Dance, Enjoy: All kinds of Dance</h1>
             <p className="p-regular-20 md:p-regular-24">Book and learn all different dance classes! Never been easier</p>
+            <Button size='lg' asChild className='button w-full'>
+              <Link href='#events'>
+                Explore more
+              </Link>
+            </Button>
           </div>
+          <Image
+            src='/assets/images/hero.png'
+            alt="hero"
+            width={1000}
+            height={1000}
+            className="max-h-[70vh] object-contain object-center 2xl:max-h-[50vh]"
+          />
+        </div>
+      </section >
+
+      <section id="events" className="wrapper my-8 flex flex-col gap-8 md:gap-12">
+        <h2 className="h2-bold">Trusted by <br /> Thousands of Events</h2>
+        <div className="flex w-full flex-col gap-5 md:flex-row">
+          Search
+          Category
         </div>
       </section>
     </>
